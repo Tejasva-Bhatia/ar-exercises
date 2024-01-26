d55b64f5066175101104b22d750230bcb54d3175
Tejasva-Bhatia <bhatiatejasva15@gmail.com>
2024-01-25 22:21:12 -0600

    2nd and 3rd exercise implemented

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,15 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+puts "Number of stores: #{Store.count}"
+
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+
+@store1.update(name: "New Store Name")
+
+@store3 = Store.find_by(id: 2)
+
+@store3.destroy
+
+puts "Number of stores after deletion: #{Store.count}"
